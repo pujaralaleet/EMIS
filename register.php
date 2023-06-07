601e12755d9969820c66c32144328c6e1e60e5f9
DESKTOP-MPK4EH5 <pujaralaleet581@gmail.com>
2023-06-07 09:41:06 +0545

    header-index-register

--- register.php
+++ register.php
@@ -1,25 +1,36 @@
 
-<html>
-  <head>
 
-  <title>register page</title>
-</head>
-
+<?php  require_once("header.php");?>
 <style>
   body {
-    background-image: url("View/img/p1.jpg");
-    background-color: #cccccc;
+    background-image: url("view/img/p1.jpeg");
+    /* background-color: #cccccc; */
+  }
+  table{
+   
+    color: purple;
+  }
+  #form-heading {
+
+    margin-top: 25px;
+    font-size: x-large;
+    font-family: cursive;
+    color: purple;
   }
 </style>
 
 <form name="iksk" method="post" action="" enctype="">
-  
 
-    <tr>
-      
-          <td width="74%" valign="top"><table width="739" height="895" border="0" align="center" cellpadding="0" cellspacing="0" bordercolor="#000033" bgcolor="#FFFFFF">
-            <tr height="">
-              <td height="auto" colspan="6" align="center" bordercolor="#CC0000" bgcolor=""></td>
+<table width="739" height="895" border="0" align="center" cellpadding="0" cellspacing="0" bordercolor="#000033" bgcolor="#FFFFFF">
+           
+<tr height="">
+              <td height="50px" colspan="6" align="center" bordercolor="#CC0000" >
+
+
+           <h3 id="form-heading">Student's Registration Form</h3> 
+          
+
+              </td>
             </tr>
             <tr>
               <td align="right" height="44">
@@ -74,7 +85,7 @@
                   <input name="nationality" type="text" size="50" placeholder="Enter the Nationality......" />
                 </div></td>
             </tr>
-            <tr>
+            <!-- <tr>
               <td height="34" align="right">
               <div align="center" class="style5 style15"><strong>Marital status</strong></div></td>
               <td>
@@ -87,7 +98,7 @@
                   </select>
                   </label>
                 </div></td>
-            </tr>
+            </tr> -->
             <tr>
               <td height="35" align="right">
               <div align="center" class="style5 style15"><strong>Address</strong></div></td>
@@ -135,7 +146,7 @@
             </tr>
             <tr>
               <td height="30" align="right">
-              <div align="center" class="style5 style15"><strong>Registraation-id</strong></div></td>
+              <div align="center" class="style5 style15"><strong>Registration-id</strong></div></td>
               <td>
                 
                 <div align="left">
@@ -171,28 +182,41 @@
             </tr>
             <tr>
               <td height="34" align="right">
-              <div align="center" class="style5 style15"><strong>Deate of issue </strong></div></td>
+              <div align="center" class="style5 style15"><strong>Date of Birth </strong></div></td>
               <td>
                 
                 <div align="left">
                   <input name="m_payment" type="date" size="50" placeholder="Enter Date of issue......" />
                 </div></td>
             </tr>
+          
             
-            <tr>
+  
             
-            </tr>
-          </table></td>
-        </tr>
-      </td>
-    </tr>
+            <tr height="">
+              <td height="50px" colspan="6" align="center" bordercolor="#CC0000" >
 
-    <button type="submit" class="btn btn-primary">submit</button><br>
+              <input type="submit" value="register" name="reg_btn"class="btn btn-outline-primary"/>
+            
+
+              </td>
+            </tr>
+          </table>
+
+ 
   
 </form>
 
 
-
 </body>
-</html>
 
+</html>
+<script src="js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+"
+  crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+
+<!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+      -->
